cc9e3cad8ab7b1c1911c99000038f572115c6bf6
Anil Krishna <anikris21@outlook.com>
2023-04-21 21:37:53 -0700

    CSV model binder

---
+++ MVCApplication/MVCApplication/Model Binding/CVSModelBinder.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using MVCApplication.Controllers;
+
+namespace MVCApplication.Model_Binding
+{
+    public class CVSModelBinder :  IModelBinder
+    {
+
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            var rawCSV = bindingContext.ValueProvider.GetValue("csv").ToString();
+            var csv = rawCSV.Split(Environment.NewLine.ToCharArray());
+
+            var createOrderList = new List<Order>();
+            foreach (var csvItem in csv)
+            {
+                var orderValues = csvItem.Split(",");
+                Order order = new Order();
+                order.ProductDescription = orderValues[2];
+                order.ProductName = orderValues[0];
+                order.Count = orderValues[1];
+                createOrderList.Add(order);
+            }
+
+            bindingContext.Result = ModelBindingResult.Success(createOrderList);
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ MVCApplication/MVCApplication/Model Binding/CVSModelBinderProvider.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using MVCApplication.Controllers;
+
+namespace MVCApplication.Model_Binding
+{
+    public class CVSModelBinderProvider : IModelBinderProvider
+    {
+        private IModelBinder? _modelBinder;
+
+        public IModelBinder GetBinder(ModelBinderProviderContext context)
+        {
+            if(context.Metadata.ModelType == typeof(List<Order>))
+            {
+                _modelBinder = new CVSModelBinder();
+            }
+            return _modelBinder;
+        }
+    }
+}
