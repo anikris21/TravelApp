bf3a7a4e57c95f1d79c3ce9d92ca54b7b77e9706
Anil Krishna <anikris21@outlook.com>
2023-04-21 20:38:09 -0700

    CSV to List

---
+++ MVCApplication/MVCApplication/Controllers/OrderController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCApplication.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Index(List<Order> order)
+        {
+            return Content("Binding success");
+        }
+    }
+
+    public class Order
+    {
+        public string? ProductName { get; set; }
+        public string? Count { get; set; }
+        public string? ProductDescription { get; set;}
+    }
+}
